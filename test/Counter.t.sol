10a6a1feb254d2ea62dbcdadbf30a5de99ea4828
parseb <petra306@protonmail.com>
2022-09-05 14:48:25 +0300

    rm diamonds, adds init test

--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -6,13 +6,33 @@ import "../src/mock/MockDelegatable.sol";
 
 contract DelegateTest is Test {
     MockDelegatable public D;
-    function setUp() public {
-       D = new MockDelegatable("DDD");
 
+    address iOwner;
+    address gotDelegation;
+
+    function setUp() public {
+       
+       iOwner = address(16);
+       gotDelegation = address(256);
+       D = new MockDelegatable("DDD");
     }
 
-    function testIncrement() public {
-        1+1;
+    function sTa(string memory s_) public returns (address){
+        return address(bytes20(bytes(s_)));
+    }
+
+    function testFunctionNoDelegation() public {
+        vm.startPrank(iOwner);
+        assertTrue(sTa(D.purpose()) == sTa("What is my purpose?"));
+        D.setPurpose("New Purpose");
+        assertTrue(sTa(D.purpose()) == sTa("New Purpose"));
+        vm.stopPrank();
+        
+        vm.prank(gotDelegation);
+        vm.expectRevert("Ownable: caller is not the owner");
+        D.setPurpose("I want my purpose to be your purpose!");
+
+
     }
 
 }
